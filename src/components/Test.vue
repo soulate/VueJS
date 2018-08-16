a9906e296306137077714591d10678dc507adf5c
DESKTOP-KFG48JQ\SouLTree <soul767@naver.com>
2018-08-17 02:14:09 +0900

    no message

---
+++ src/components/Test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>{{name}}</p>
+    <button>버튼</button>
+  </div>
+</template>
+
+<script lang="ts">
+  //import Vue from 'vue'
+  //import Component from 'vue-class-component'
+
+  import { Vue, Component, Prop } from 'vue-property-decorator';
+
+  @Component
+  export default class TestComponent extends Vue {
+    // data
+    name: string = 'World'
+
+  }
+</script>
+
+<style scoped>
+  p {
+    color: blue;
+  }
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import HelloComponent from './components/Hello.vue';
+import TestComponent from './components/Test.vue';
 
 new Vue({
   el: '#app',
   template: `
     <div>
+      <!--
       <div>Hello {{name}}!</div>
       Name: <input type="text" v-model="name">
       <hello-component :name="name" :initialEnthusiasm="5" />
+      -->
+      <hello-component :name="name" :initialEnthusiasm="5" />
+      <test-component/>
     </div>
   `,
   data: {
     name: 'World'
   },
   components: {
-    HelloComponent
+    HelloComponent,
+    TestComponent
   }
 })
