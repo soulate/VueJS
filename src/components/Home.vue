0ee3336d35ca48bafcf252ab4d3133f0499a003f
SMILEGATE\es-kkm <soul767@naver.com>
2018-08-28 18:09:22 +0900

    [ADD] AXIOS ENV

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tabcontent">
     <h2 v-if="title != ''">{{title}}</h2>
+    <span id="txt"></span>
   </div>
 </template>
 
@@ -16,6 +17,12 @@
 
     beforeMount () {
       this.title = this.$route.query.title || 'Home';
+
+      const api = 'https://raw.githubusercontent.com/joshua1988/doit-vuejs/master/data/demo.json';
+      this.axios.get(api).then((response) => {
+        document.getElementById('txt').innerText = JSON.stringify(response.data);
+      })
+
     },
 
   }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
+import 'es6-promise/auto';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './js/router'; //라우터 설정.
+
+Vue.use(VueAxios, axios);
 
 new Vue({
   el: '#app',
